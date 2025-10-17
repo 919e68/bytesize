bcd61f8b8f113d933977609305900aeb30afc34d
Wilson Anciro <konekred@gmail.com>
2025-10-18 00:38:12 +0800

    ui: remove unused boilerplate files

--- ui/src/lib/options/queueTypeOptions.ts
+++
@@ -1,9 +0,0 @@
-import { WalletQueueTypeEnum } from '~/graphql/generated/graphql'
-
-export const queueTypeOptions = () => {
-  return Object.keys(WalletQueueTypeEnum).map((key) => ({
-    id: WalletQueueTypeEnum[key as keyof typeof WalletQueueTypeEnum],
-    text: key,
-    value: WalletQueueTypeEnum[key as keyof typeof WalletQueueTypeEnum]
-  }))
-}
